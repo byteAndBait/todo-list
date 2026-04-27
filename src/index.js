6f6a0a40d6fdca1418a8a30847287be186d975c7
byteAndBait <abdulrahman.elkassas1@gmail.com>
2026-04-27 13:11:38 +0300

    feat: Add LocalStorage Functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import "./css/main.css"
 import "./css/normalize.css"
-import { todoEdit,projectAddTodo,projectRemoveTodo } from "./compositors.js"
+import { todoEdit, projectAddTodo, projectRemoveTodo } from "./compositors.js"
 const TODOLIST = (() => {
+    let Projects = {}
+
+    const saveToLocal = () => {
+        localStorage.setItem("projects", JSON.stringify(Projects))
+    }
+    const clearLocal = () => {
+        localStorage.clear("projects")
+        syncLocal()
+    }
+    const syncLocal = () => {
+        if (!localStorage.getItem("projects")) {
+            return "Nothing is saved in LocalStorage"
+        }
+        let localProjects = JSON.parse(localStorage.getItem("projects"))
+        for (let project in localProjects) {
+            project = localProjects[project]
+            project = Object.assign(
+                project,
+                projectAddTodo(project),
+                projectRemoveTodo(project)
+            )
+            for (let todo in project.todos) {
+                todo = project.todos[todo]
+                todo = Object.assign(
+                    todo,
+                    todoEdit(todo)
+                )
+            }
+        }
+        // Merge current Projects with localStorage
+        Projects = Object.assign(Projects, localProjects)
+    }
     class Todo {
         constructor(details) {
             this.title = details.title
@@ -9,17 +41,16 @@ const TODOLIST = (() => {
             this.priority = details.priority || "Low"
         }
     }
-    class Project{
-        constructor(name){
+    class Project {
+        constructor(name) {
             this.name = name
             this.todos = {}
         }
     }
-    const Projects = {}
-    const createProject = (name)=>{
-        if(Projects[name]){
+    const createProject = (name) => {
+        if (Projects[name]) {
             return "Project Already Exists!"
-        }else{
+        } else {
             const projectToBeCreated = new Project(name)
             Projects[name] = Object.assign(
                 projectToBeCreated,
@@ -31,59 +62,78 @@ const TODOLIST = (() => {
 
     createProject("default")
 
-    const createTodo = (details,projectName) => {
-        if(!projectName) projectName = "default";
+
+    // functions That modify the Projects object
+
+    const createTodo = (details, projectName) => {
+        syncLocal()
+        if (!projectName) projectName = "default";
         const todoToBeCreated = new Todo(details)
-        Projects[projectName].addTodo(Object.assign(todoToBeCreated,todoEdit(todoToBeCreated)))
+        Projects[projectName].addTodo(Object.assign(todoToBeCreated, todoEdit(todoToBeCreated)))
+        saveToLocal()
     }
 
-    const EditATodo = (projectName,todoTitle,dataName,modification)=>{
-        if(!projectName || !todoTitle || !dataName || !modification){
+    const EditATodo = (projectName, todoTitle, dataName, modification) => {
+        syncLocal()
+        if (!projectName || !todoTitle || !dataName || !modification) {
             return "Please Re-Check Your arguments"
         }
-        const currentTodo = _getATodo(projectName,todoTitle)
-        
-        currentTodo.edit(dataName,modification)
+        const currentTodo = _getATodo(projectName, todoTitle)
+
+        currentTodo.edit(dataName, modification)
+        saveToLocal()
     }
 
-    const _getATodo = (projectName,todoTitle)=>{
-        try{
-           Projects[projectName].todos[todoTitle] 
-        }catch(error){
+    const removeATodo = (projectName, todoTitle) => {
+        syncLocal()
+        Projects[projectName].removeTodo(todoTitle)
+        saveToLocal()
+    }
+
+
+    // Internal use only functions
+    const _getATodo = (projectName, todoTitle) => {
+        syncLocal()
+        try {
+            Projects[projectName].todos[todoTitle]
+        } catch (error) {
             return
         }
         const todoToGet = Projects[projectName].todos[todoTitle]
         return todoToGet
     }
-    const viewATodo = (projectName,todoTitle)=>{
-        if(_getATodo(projectName,todoTitle)){
-            return JSON.parse(JSON.stringify(Object.assign({},_getATodo(projectName,todoTitle))))
+
+
+    // View Data Function (Doesn't affect the Projects object)
+    const viewATodo = (projectName, todoTitle) => {
+        syncLocal()
+        if (_getATodo(projectName, todoTitle)) {
+            return JSON.parse(JSON.stringify(Object.assign({}, _getATodo(projectName, todoTitle))))
         }
         return "Your Todo Doesn't Exist!"
-        
-    }
-    const removeATodo = (projectName,todoTitle)=>{
-        Projects[projectName].removeTodo(todoTitle)
+
     }
 
-    const viewAProject = (projectName)=>{
-        if(Projects[projectName]){
+    const viewAProject = (projectName) => {
+        syncLocal()
+        if (Projects[projectName]) {
             return JSON.parse(
                 JSON.stringify(
-                    Object.assign({},Projects[projectName])
+                    Object.assign({}, Projects[projectName])
                 )
             )
         }
         return "Your Project Doesn't Exist!"
     }
-    const viewAllProjects = ()=>{
+    const viewAllProjects = () => {
+        syncLocal()
         return JSON.parse(
             JSON.stringify(
-                Object.assign({},Projects)
+                Object.assign({}, Projects)
             )
         )
     }
-    return {createTodo,createProject,EditATodo,viewATodo,removeATodo,viewAProject,viewAllProjects}
+    return { createTodo, createProject, EditATodo, viewATodo, removeATodo, viewAProject, viewAllProjects, syncLocal, saveToLocal, clearLocal }
 })()
 
 const myTodoDetails = {
@@ -96,14 +146,17 @@ const myOtherTodoDetails = {
     description: "Pretty Fascinating",
     priority: "High"
 }
-TODOLIST.createTodo(myTodoDetails)
-TODOLIST.createProject("Studying")
-TODOLIST.createTodo(myOtherTodoDetails,"Studying")
+const dumbData = () => {
+    TODOLIST.createTodo(myTodoDetails)
+    TODOLIST.createProject("Studying")
+    TODOLIST.createTodo(myOtherTodoDetails, "Studying")
 
-TODOLIST.EditATodo("default",myTodoDetails.title,"description","It's actually pretty challenging")
-console.log(TODOLIST.viewATodo("default",myTodoDetails.title))
-console.log(TODOLIST.viewATodo("Studying",myOtherTodoDetails.title))
+    TODOLIST.EditATodo("default", myTodoDetails.title, "description", "It's actually pretty challenging")
+    console.log(TODOLIST.viewATodo("default", myTodoDetails.title))
+    console.log(TODOLIST.viewATodo("Studying", myOtherTodoDetails.title))
 
-TODOLIST.removeATodo("Studying",myOtherTodoDetails.title)
-console.log(TODOLIST.viewATodo("Studying",myOtherTodoDetails.title))
-console.log(TODOLIST.viewAllProjects())
+    TODOLIST.removeATodo("Studying", myOtherTodoDetails.title)
+    console.log(TODOLIST.viewATodo("Studying", myOtherTodoDetails.title))
+}
+TODOLIST.clearLocal()
+dumbData()
